0ec0d405ac28d6351d962e07c38591f66d52ee2e
bagleric <bagleric@outlook.com>
2017-09-23 20:45:07 -0600

    Added Home page

---
+++ web/JS/02TeachActivity.js
@@ -0,0 +1,8 @@
+function onBtnClicked(){
+    alert("Clicked!");
+};
+
+function changeColor(){
+var theColor = document.getElementById("colortxt").value;
+document.getElementById("div1").style.backgroundColor = theColor;
+}
---
+++ web/hello.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+
+<body>
+  <p>
+<?php
+  echo 'Hello from PHP!';
+      
+      $num = 4+"cat";
+      echo $num;
+      
+      
+      $value = 4;
+      print "the value is $value"; 
+          
+          $x = 4;
+$y = "4";
+print "1";
+
+if ($x == $y)
+print "2";
+if ($x === $y)
+print "3";
+?>
+  </p>
+</body>
+</html>
